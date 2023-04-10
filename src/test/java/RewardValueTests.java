0602aca48e90d27a45b6193785541c007edd335b
risa-10 <106301648+risa-10@users.noreply.github.com>
2023-04-10 10:14:05 -0700

    added two unit tests

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -3,6 +3,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RewardValueTests {
+    public static final double MILES_TO_CASH_CONVERSION_RATE = 0.0035;
 
     @Test
     void create_with_cash_value() {
@@ -20,11 +21,17 @@ public class RewardValueTests {
 
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
+        double cashValue = 100;
+        int valueInMiles = (int) (cashValue / MILES_TO_CASH_CONVERSION_RATE);
+        var rewardValue = new RewardValue(cashValue);
+        assertEquals(valueInMiles, rewardValue.getMilesValue());
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
+        int milesValue = 1000;
+        double valueInCash = milesValue * MILES_TO_CASH_CONVERSION_RATE;
+        var rewardValue = new RewardValue(milesValue);
+        assertEquals(valueInCash, rewardValue.getCashValue());
     }
 }
